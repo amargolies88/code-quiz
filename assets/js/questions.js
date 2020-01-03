15d070cf865a0a71a4442427d614f10c81182061
Alan Margolies <amargolies88@gmail.com>
2020-01-03 13:23:16 -0500

    Added questions.js and barebone splash page with start button

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<scripting>", "<javascript>", "<script>", "<js>"],
+      answer: "<script>"
+    },
+    {
+      title: "Where is the correct place to insert a JavaScript?",
+      choices: ["The <body> section", "The <head> section", "Both the <head> section and the <body> section are correct"],
+      answer: "<script>"
+    },
+  ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,34 @@
-var mainContainer = document.createElement("div");
-document.appendChild(mainContainer);
+//Building Main Container
+var mainContainer = $("<div>");
+mainContainer.addClass("container-fluid");
+$(document.body).append(mainContainer);
+
+//Building Banner
+var banner = $("<div>").addClass("row no-gutters");
+mainContainer.append(banner);
+
+var bannerText = $("<h1>JavaScript Quiz</h1>");
+banner.append(bannerText);
+
+//Build Button Splash Area
+var mainRow = $("<div>").addClass("row no-gutters");
+mainContainer.append(mainRow);
+
+var splashButtonStart = $("<button>Start</button>").addClass("btn btn-primary");
+mainRow.append(splashButtonStart);
+
+splashButtonStart.click(startQuiz);
+
+
+function startQuiz(){
+    splashButtonStart.remove();
+
+    for (let i = 0; i < questions.length; i++) {
+        
+
+
+
+    }
+    //Build Question Title
+
+}
