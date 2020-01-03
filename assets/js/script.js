fecdc3c5c0260f51e27f5e30a1df33005855dbdf
Alan Margolies <amargolies88@gmail.com>
2020-01-03 15:14:35 -0500

    added quiz function. quiz works (barebones)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,83 @@
 //Building Main Container
 var mainContainer = $("<div>");
-mainContainer.addClass("container-fluid");
+mainContainer.addClass("container-fluid p-sm-5");
 $(document.body).append(mainContainer);
 
 //Building Banner
-var banner = $("<div>").addClass("row no-gutters");
+var banner = $("<div>").addClass("row no-gutters banner-row");
 mainContainer.append(banner);
 
 var bannerText = $("<h1>JavaScript Quiz</h1>");
 banner.append(bannerText);
 
 //Build Button Splash Area
-var mainRow = $("<div>").addClass("row no-gutters");
-mainContainer.append(mainRow);
+var questionRow = $("<div>").addClass("row no-gutters question-row");
+mainContainer.append(questionRow);
 
 var splashButtonStart = $("<button>Start</button>").addClass("btn btn-primary");
-mainRow.append(splashButtonStart);
+questionRow.append(splashButtonStart);
 
 splashButtonStart.click(startQuiz);
 
 
-function startQuiz(){
+function startQuiz() {
     splashButtonStart.remove();
+    let takingQuiz = true;
+    let timer = 0;
+    let questionIndex = 0;
 
-    for (let i = 0; i < questions.length; i++) {
-        
+    let questionTitleCol = $("<div>").addClass("col question-title-col border");
+    questionRow.append(questionTitleCol);
 
+    let questionText = $("<h4>").addClass("question-");
+    questionTitleCol.append(questionText);
 
+    let timerCol = $("<div>").addClass("col-auto border");
+    questionRow.append(timerCol);
 
+    let timerText = $("<span>").addClass("timer-text");
+    timerCol.append(timerText);
+
+    setInterval(
+        function () {
+            timerText.text((timer += 0.1).toFixed(1));
+        },
+        100
+    );
+
+    //Create Answer Row
+    let answerRow = $("<div>").addClass("row no-gutters answer-row");
+    mainContainer.append(answerRow);
+
+    let correct = 0;
+    let wrong = 0;
+
+    loadQuestion();
+
+    function loadQuestion() {
+        answerRow.empty();
+        let question = questions[questionIndex];
+        questionText.text(question.title);
+
+        question.choices.forEach(choice => {
+            let choiceCol = $("<div>").addClass("col border");
+            let choiceText = $("<span>").addClass("choice");
+            choiceText.text(choice);
+            choiceCol.append(choiceText);
+            answerRow.append(choiceCol);
+            choiceCol.click(function () {
+                if (this.children[0].textContent == question.answer) {
+                    correct++;
+                } else {
+                    wrong++;
+                }
+                if (questionIndex < (questions.length - 1)) {
+                    questionIndex++;
+                    loadQuestion();
+                } else {
+                    endQuiz();
+                }
+            });
+        });
     }
-    //Build Question Title
-
 }
